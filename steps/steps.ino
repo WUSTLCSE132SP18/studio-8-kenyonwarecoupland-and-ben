ed2073d061e2591f349a57f75e8fb14e9c7c83d6
Ben <Ben@DESKTOP-A1PMVAA.dhcp.wustl.edu>
2018-03-19 11:52:24 -0700

    ben hansen

---
+++ steps/steps.ino
@@ -0,0 +1,56 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+MMA8452Q accel;
+void setup() {
+  Serial.begin(9600);
+  accel.init();
+}
+void loop()
+{
+  if (accel.available())
+  {
+    accel.read();
+    
+    printCalculatedAccels();
+    
+    //printOrientation();
+    
+    Serial.println();
+  }
+}
+void printCalculatedAccels()
+{ 
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.print(accel.cz, 3);
+  //Serial.print(",");
+}
+void printOrientation()
+{
+  // accel.readPL() will return a byte containing information
+  // about the orientation of the sensor. It will be either
+  // PORTRAIT_U, PORTRAIT_D, LANDSCAPE_R, LANDSCAPE_L, or
+  // LOCKOUT.
+  byte pl = accel.readPL();
+  switch (pl)
+  {
+  case PORTRAIT_U:
+    Serial.print("Portrait Up");
+    break;
+  case PORTRAIT_D:
+    Serial.print("Portrait Down");
+    break;
+  case LANDSCAPE_R:
+    Serial.print("Landscape Right");
+    break;
+  case LANDSCAPE_L:
+    Serial.print("Landscape Left");
+    break;
+  case LOCKOUT:
+    Serial.print("Flat");
+    break;
+  }
+}
